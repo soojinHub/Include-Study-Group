06a4a73cb1d8ad251e332fb0290092113c85fec6
soojinHub <51077100+soojinHub@users.noreply.github.com>
2021-01-19 21:44:20 +0900

    Create 03_14921_용액합성하기.c
    오답

---
+++ 1주차_C문제/03_14921_용액합성하기.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int abs(int a) {
+	return a > 0 ? a : -a;
+}
+
+int main(void) {
+	int num;
+	int arr[100000] = { 0, };
+
+	scanf("%d", &num);
+	for (int i = 0; i < num; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	int left = 0; int right = num - 1;
+	int min = abs(arr[left] + arr[right]);
+	while (left < right) {
+		int sign = 0;
+		if ((min >= abs(arr[left] + arr[right - 1])) && (left != right - 1)) {
+			min = abs(arr[left] + arr[right - 1]);
+			right--;
+			sign = 1;
+
+		}
+		if ((min >= abs(arr[left + 1] + arr[right])) && (left + 1 != right)) {
+			min = abs(arr[left + 1] + arr[right]);
+			left++;
+			sign = 1;
+		}
+		if ((sign==0)&&(left + 1 != right-1)){
+			min = abs(arr[left + 1] + arr[right - 1]);
+			left++;
+			right--;
+			sign = -1;
+		}
+		int min = abs(arr[left] + arr[right]);
+		if (sign == 0)
+			break;
+	}
+	printf("%d", min);
+	return 0;
+}
