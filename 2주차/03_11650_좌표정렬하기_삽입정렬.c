ccad7b37d5dbc731159182d29bda53ce6f69442b
soojinHub <51077100+soojinHub@users.noreply.github.com>
2021-01-26 16:28:17 +0900

    스터디 2-3 풀이

---
+++ 2주차/03_11650_좌표정렬하기_삽입정렬.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct point
+{
+    int x;
+    int y;
+}Point;
+
+void insertion_sort(Point arr[], int n);
+void swap(Point* a, Point *b);
+
+int main(void){
+    int num;
+    scanf("%d", &num);
+    Point * arr = (Point*)malloc(sizeof(Point)*num); //구조체 배열 메모리 할당
+    
+    for (int i = 0; i<num; i++){
+        scanf("%d %d", &arr[i].x, &arr[i].y);
+    }
+    
+    insertion_sort(arr, num);
+
+    for (int i = 0; i<num; i++){
+        printf("%d %d\n", arr[i].x, arr[i].y);
+    }
+return 0;
+}
+
+void insertion_sort(Point arr[], int n){
+    for (int i = 1; i<n; i++){
+        for (int j=i; j>0;j--){
+            if (arr[j-1].x > arr[j].x) swap(&arr[j-1], &arr[j]);
+            else if ((arr[j-1].x == arr[j].x)&&(arr[j-1].y>arr[j].y)) swap(&arr[j-1],&arr[j]);
+        }
+    }
+
+}
+
+void swap(Point *a, Point *b){
+    Point tmp = *a;
+    *a = *b;
+    *b= tmp;
+}
---
+++ 2주차/03_11650_좌표정렬하기_합병정렬.c
@@ -0,0 +1,64 @@
+//정답
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct point
+{
+    int x;
+    int y;
+}Point;
+
+void merge(Point arr[], Point temp[], int l, int m, int r);
+int merge_sort(Point arr[], Point temp[], int l, int r);
+void swap(Point* a, Point *b);
+
+int main(void){
+    int num;
+    scanf("%d", &num);
+    Point * arr = (Point*)malloc(sizeof(Point)*num); //구조체 배열 메모리 할당
+    Point * temp = (Point*)malloc(sizeof(Point)*num);
+    for (int i = 0; i<num; i++){
+        scanf("%d %d", &arr[i].x, &arr[i].y);
+    }
+    
+    merge_sort(arr, temp, 0, num-1);
+
+    for (int i = 0; i<num; i++){
+        printf("%d %d\n", arr[i].x, arr[i].y);
+    }
+return 0;
+}
+
+void merge(Point arr[], Point temp[], int l, int m, int r){
+    int i = l;
+    int j = m+1;
+    for (int k =l; k<r+1;k++){
+        if (i>m){
+            temp[k]=arr[j];
+            j++;
+        }
+        else if (j>r){
+            temp[k]=arr[i];
+            i++;
+        }
+        else if ((arr[j].x<arr[i].x)||((arr[j].x==arr[i].x)&&(arr[j].y<arr[i].y))){
+            temp[k] = arr[j];
+            j++;
+        }
+        else{
+            temp[k]=arr[i];
+            i++;
+        }
+    }
+    for (int k =l; k<r+1; k++){
+        arr[k]=temp[k];
+    }
+}
+
+int merge_sort(Point arr[], Point temp[], int l, int r){
+    if (r<=l) return -1; //arr의 크기가 1이라면 종료
+    int m = l + (r-l)/2; //중간 원소의 인덱스
+    merge_sort(arr, temp, l, m);
+    merge_sort(arr, temp, m+1, r);
+    merge(arr, temp, l, m, r);
+}
