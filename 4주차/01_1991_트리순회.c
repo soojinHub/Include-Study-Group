aa6fde59aae52866afdbc45d8dfe925d518fcf66
Soojin Lee <soojinyi9825@gmail.com>
2021-02-07 02:09:09 +0900

    3-1 문제풀이

--- 4주차/01_1991_트리순회.c
+++ 4주차/01_1991_트리순회.c
@@ -1,43 +1,50 @@
 #include <stdio.h>
 
-int arr[26][2]={'.',};
+char arr[26][2]={'.',};
 
-void preorder(int root);
-void inorder(int root);
-void postorder(int root);
+void preorder(char root);
+void inorder(char root);
+void postorder(char root);
 
 int main(void){
-    int num, root, left, right;
+    int n = 10;
+    int num;
+    char root, left, right;
     scanf("%d", &num);
+    
     for (int i =0; i<num; i++){
-        scanf("%d %d %d", &root, &left, &right);
-        arr[root-'A'][0]=left;
-        arr[root-'A'][1]=right;
+        getchar();
+        scanf("%c %c %c", &root, &left, &right);
+        arr[(int)root-'A'][0]=left;
+        arr[(int)root-'A'][1]=right;
     }
-    preorder(0);
-    inorder(0);
-    postorder(0);
+    
+    preorder('A');
+    printf("\n");
+    inorder('A');
+    printf("\n");
+    postorder('A');
     return 0;
 }
 
-void preorder(int root){
+void preorder(char root){
     if (root == '.')
         return;
-    printf("%d, root");
-    preorder(arr[root-'A'][0]);
-    preorder(arr[root-'A'][1]);
+    printf("%c", root);
+    preorder(arr[(int)root-'A'][0]);
+    preorder(arr[(int)root-'A'][1]);
 }
-void inorder(int root){
+void inorder(char root){
     if (root == '.')
         return;
-    inorder(arr[root-'A'][0]);        
-    printf("%d, root");
-    inorder(arr[root-'A'][1]);
+    inorder(arr[(int)root-'A'][0]);        
+    printf("%c", root);
+    inorder(arr[(int)root-'A'][1]);
 }
-void postorder(int root){
+void postorder(char root){
     if (root == '.')
         return;
-    postorder(arr[root-'A'][0]);
-    postorder(arr[root-'A'][1]);
-    printf("%d, root");
+    postorder(arr[(int)root-'A'][0]);
+    postorder(arr[(int)root-'A'][1]);
+    printf("%c", root);
 }
